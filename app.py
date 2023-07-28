1dcd724ab61c0d5303c86fe98757b4e7632b9930
Chris Ross <chrisross00@gmail.com>
2023-07-28 06:38:54 -0700

    - add model config handling to app

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 from flask import Flask, request, render_template
-from transformers import BertForQuestionAnswering, BertTokenizer, pipeline
+from transformers import BertForQuestionAnswering, BertTokenizer, RobertaForQuestionAnswering, RobertaTokenizer, AutoTokenizer, AutoModelForQuestionAnswering, pipeline
 import torch
+import json
 
 app = Flask(__name__)
 
+# Load the configuration file
+with open('config.json') as f:
+    config = json.load(f)
+
+# Get the model name from the config
+model_name = config['model_name']
+
+# Check the model type and load the appropriate model and tokenizer
+if model_name == 'bert-base-uncased':
+    tokenizer = BertTokenizer.from_pretrained(model_name)
+    model = BertForQuestionAnswering.from_pretrained(model_name)
+elif model_name == 'roberta-base':
+    tokenizer = RobertaTokenizer.from_pretrained(model_name)
+    model = RobertaForQuestionAnswering.from_pretrained(model_name)
+else:
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+
 # Specify the path to your saved model
-model_path = "model.pth"
-
-# Specify the path to your BERT base model
-bert_base_model_path = 'bert-base-uncased'
-
-# Load the tokenizer
-tokenizer = BertTokenizer.from_pretrained(bert_base_model_path)
-
-# Load the BERT QA Model
-model = BertForQuestionAnswering.from_pretrained(bert_base_model_path)
+model_path = "D:\AI Projects\llama\Llama-2-70b-hf\model.safetensors.index"
 
 # Load the state dict of your saved model
 model.load_state_dict(torch.load(model_path))
